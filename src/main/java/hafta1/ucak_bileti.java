c1a282fcdbc040d47bceced280eedaa42068badd
gocmensuheda <suheda.gocmen@hotmail.com>
2025-03-18 14:52:11 +0300

    uçak bileti uygulamasına kullanıcıdan alınan değerlerin pozitifliği kontrolü kodu eklendi.

--- src/main/java/hafta1/ucak_bileti.java
+++ src/main/java/hafta1/ucak_bileti.java
@@ -15,32 +15,37 @@ public class ucak_bileti {
         System.out.println("Yolculuk tipini giriniz (1 => Tek Yön , 2 => Gidiş Dönüş ): 2 ");
         int tip = scanner.nextInt();
 
+
+
         double normalTutar = mesafe * 0.10;
         double yasIndirimOrani = 0; //yaş indirimi kontrolü.
-        if (yas<12){
-            yasIndirimOrani= 0.50;
-        }else if(yas<24){
-            yasIndirimOrani= 0.10;
-        }else if(yas>65){
-            yasIndirimOrani=0.30;
+        if(mesafe>0 && yas>0 && (tip==1 || tip ==2)){
+            if (yas<12){
+                yasIndirimOrani= 0.50;
+            }else if(yas<24){
+                yasIndirimOrani= 0.10;
+            }else if(yas>65){
+                yasIndirimOrani=0.30;
+            }
+            double yasIndirimi = normalTutar * yasIndirimOrani;
+            double indirimliTutar = normalTutar - yasIndirimi;
+
+            double tipIndirimOrani = 0; // yolculuk tipine göre indirim yapılacak mı yapılmayacak mı kontrolü.
+
+            switch(tip){
+                case 1:
+                    tipIndirimOrani = 0;
+                    break;
+                case 2:
+                    tipIndirimOrani = 0.20;
+                    break;
+            }
+            double tipIndirimi = indirimliTutar * tipIndirimOrani;
+            double tipIndirimliTutar = indirimliTutar - tipIndirimi;
+
+            System.out.println("Toplam Ödenecek tutar: "+ tipIndirimliTutar);
         }
-        double yasIndirimi = normalTutar * yasIndirimOrani;
-        double indirimliTutar = normalTutar - yasIndirimi;
 
-        double tipIndirimOrani = 0; // yolculuk tipine göre indirim yapılacak mı yapılmayacak mı kontrolü.
-
-        switch(tip){
-            case 1:
-                tipIndirimOrani = 0;
-                break;
-            case 2:
-                tipIndirimOrani = 0.20;
-                break;
-        }
-        double tipIndirimi = indirimliTutar * tipIndirimOrani;
-        double tipIndirimliTutar = indirimliTutar - tipIndirimi;
-
-        System.out.println("Toplam Ödenecek tutar: "+ tipIndirimliTutar);
 
 
 
