1cd03f9ea0115b33dc945e11c9156554c27ab10d
gocmensuheda <suheda.gocmen@hotmail.com>
2025-03-24 16:51:36 +0300

    transpoz kodu eklendi...

--- src/main/java/hafta1/matris_transpoz.java
+++ src/main/java/hafta1/matris_transpoz.java
@@ -11,7 +11,7 @@ public class matris_transpoz {
         for (int i = 0; i < matris.length; i++) {
             for (int j = 0; j < matris[i].length; j++) {
                 System.out.print(matris[i][j] + " ");
-                //burda transpoz işlemi yapılıyor. satırlar ve sütunlar yer değiştiriyor.
+
 
             }
             System.out.println();
