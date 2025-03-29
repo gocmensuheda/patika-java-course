3f1708d5d3d1dd9029f1d06eb088c9814aeab6c9
gocmensuheda <suheda.gocmen@hotmail.com>
2025-03-29 03:01:44 +0300

    asal sayı kodu eklendi.

--- src/main/java/hafta2/asal_sayi.java
+++ src/main/java/hafta2/asal_sayi.java
@@ -8,23 +8,36 @@ public class asal_sayi {
         System.out.println("Bir sayı giriniz: ");
         int sayi=scanner.nextInt();
 
-       int sonuc = asalSayi(sayi,2);
+      /* int sonuc = asalSayi(sayi,2); // bu asalSayi methodu için yazıldı.
        if(sonuc==1) System.out.println("bu bir asal sayıdır.");
-       else System.out.println("bu bir asal sayı değildir.");
+       else System.out.println("bu bir asal sayı değildir.");*/
+
+        boolean sonuc=asal(sayi,2);
+        if (sonuc==true) System.out.println("asal sayıdır.");
+        else System.out.println("asal sayı değildir.");
 
 
     }
     private static int asalSayi(int sayi, int bolen){
+
         if (sayi <= 1) {
-            return 0; // 1 ve daha küçük sayılar asal değildir.
+            return  0; // 1 ve daha küçük sayılar asal değildir.
         }
         if (bolen >= sayi) {
-            return 1; // Tüm bölenler kontrol edildi, sayı asaldır.
+            return 1 ; // Tüm bölenler kontrol edildi, sayı asaldır.
         }
         if (sayi % bolen == 0) {
             return 0; // Tam bölünme varsa asal değildir.
         }
         return asalSayi(sayi, bolen+1);
 
+    }
+
+    private static boolean asal(int sayi, int bolen){ //iki farklı method yazdım ikisi ile de çalışır.
+        if(sayi<=0) return false;
+        if(bolen>=sayi) return true;
+        if(sayi%bolen ==0) return false;
+
+        return asal(sayi,bolen+1);
     }
 }
