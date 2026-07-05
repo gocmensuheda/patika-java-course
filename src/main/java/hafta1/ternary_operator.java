520dda6a13f3bbaf884f39e8d1ae8e7141a29413
gocmensuheda <suheda.gocmen@hotmail.com>
2026-07-05 22:37:12 +0300

    Ternary Operator Example

---
+++ src/main/java/hafta1/ternary_operator.java
@@ -0,0 +1,11 @@
+package hafta1;
+
+public class ternary_operator {
+    public static void main(String[] args) {
+        String message = "";
+        int number=13;
+        message= number>12 ? "The number is greater than 12.": "The number is less than 12";
+
+        System.out.printf(message);
+    }
+}
