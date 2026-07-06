93cf3dfe40c225b24f84b70463034fc552165eaf
gocmensuheda <suheda.gocmen@hotmail.com>
2026-07-07 00:51:48 +0300

    Do While Example

---
+++ src/main/java/hafta1/Loops/DoWhile.java
@@ -0,0 +1,24 @@
+package hafta1.Loops;
+
+import java.util.Scanner;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String password ="1234";
+        int count=0;
+        System.out.println("Please enter your password:");
+        String input = scanner.nextLine();
+        do {
+            if(input.equals(password)) {
+                System.out.println("Password is correct");
+                break;
+            }
+            else {
+                System.out.println("Password is not correct");
+                count++;
+            }
+
+        }while(count<3);{}
+    }
+}
